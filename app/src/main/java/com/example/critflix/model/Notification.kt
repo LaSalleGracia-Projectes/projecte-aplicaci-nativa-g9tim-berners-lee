4391e00fa15822b57483a639360f43c4f18669cb
XCDavidXD2 <davidgustavo.correa@gracia.lasalle.cat>
2025-04-26 02:29:45 +0200

    feat: model para las notificaciones usando la bbdd

--- app/src/main/java/com/example/critflix/model/Notification.kt
+++ app/src/main/java/com/example/critflix/model/Notification.kt
@@ -1,8 +1,14 @@
 package com.example.critflix.model
 
+import com.google.gson.annotations.SerializedName
+import java.util.Date
+
 data class Notification(
     val id: Int,
-    val title: String,
-    val message: String,
-    val imageUrl: String
+    @SerializedName("user_id") val userId: Int,
+    val mensaje: String,
+    val tipo: String,
+    val leido: Boolean,
+    @SerializedName("created_at") val createdAt: String,
+    @SerializedName("updated_at") val updatedAt: String
 )
